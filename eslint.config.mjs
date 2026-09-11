c27b9371ed817c957d2c49e6126d0c9cdd39a57e
Connor Harvey <connorharvey803@gmail.com>
2026-09-10 19:15:21 -0600

    Added eslint and CI pipeline.

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import jest from "eslint-plugin-jest";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ['**/*.test.js', '**/*.spec.js'],
+    ...jest.configs['flat/recommended'],
+  },
+]);
